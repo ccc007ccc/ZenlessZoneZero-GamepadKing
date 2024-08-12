df1ec16b91954442d15c3cdc3d7ecac66201c3d0
ccc007 <1492949083@qq.com>
2024-08-12 22:23:15 +0800

    更新README以详细说明安装步骤和已知问题

---
+++ 启动.bat
@@ -0,0 +1,27 @@
+@echo off
+chcp 65001
+:: 获取管理员权限
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+    echo 请使用管理员权限启动
+    pause
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+start /b AutoConfiguration.exe args
+
+popd
